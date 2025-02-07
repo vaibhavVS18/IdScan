08088e768036c72c87dc4c5623a73759cb4db0ac
vaibhav <vaibhav.iiituna1111@gmail.com>
2025-02-07 17:06:13 +0530

    google oath url updated

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, (accessToken, refreshToken, profile, done) => {
   // Here, you can store user info in DB
   return done(null, profile);
